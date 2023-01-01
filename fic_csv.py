dcd4c71c4fdf64ab9e8d4742786bff1bdb6c9cf3
numero6 <numero6@therover.thevillage>
2023-01-01 21:17:43 +0100

    Modification sortie

--- fic_csv.py
+++ fic_csv.py
@@ -8,6 +8,5 @@ with open('freq.csv') as fichier_csv:
     reader = csv.DictReader(fichier_csv, delimiter=',')
     #reader = csv.reader(fichier_csv, delimiter=',')
     for ligne in reader:
-       #print ("Canal: " + ligne['Canal'] + " Fréquence CB: " + ligne['Normaux'] + " Fréquence 1x Sup: " + ligne['1x Supérieur'])
-        print (ligne['Canal'])
-        
+       print ("Canal: " + ligne['Canal'] + " Fréquence CB: " + ligne['Normaux'] + " Fréquence 1x Sup: " + ligne['1x Supérieur'])
+       #print (ligne['Canal'])
