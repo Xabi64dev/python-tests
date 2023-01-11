1a9f5b0cabbdc81cedbbb8a8abd22ad717e628e0
numero6 <numero6@therover.thevillage>
2023-01-11 01:59:35 +0100

    Merge branch 'main' of https://github.com/Xabi64dev/python

--- data_read_cdc_mpj.py
+++ data_read_cdc_mpj.py
@@ -2,12 +2,11 @@
 # coding: utf-8
 
 import csv
-import re
 
 with open('data_cdc_mpj.csv') as fichier_csv:
     # taille = len(fichier_csv.readlines())
     reader = csv.DictReader(fichier_csv,delimiter=',')
-    motif = input("Motif recherché.... : ")
+    motif = input("Motif recherché.... ref or category: ")
     
     # taille = len(fichier_csv.readlines())
     # print(taille)
@@ -19,15 +18,3 @@ with open('data_cdc_mpj.csv') as fichier_csv:
             print("Ref: " + ligne['ref'] + "\nCategory: " + ligne['cat'] + "\nMaterial:\n" + ligne['material'] + "\n" + "*" * 31)
         else:
             continue
-       #print(ligne.count(motif))
-       #print(len(ligne.get("material").split("\n")))
-    #print(ligne.get("material").split("\n")[0:])
-       
-""" if motif in ligne.values():
-            print(ligne.values())
-            print(ligne.items())
-            print("\n"+"*" * 31)
-            print("Ref: " + ligne['ref'] + "\nCategory: " + ligne['cat'] + "\nMaterial: " + "\n" + ligne['material'])
-            print(ligne.get("material").split("\n"))
-        if motif in ligne.get("material").split("\n"):
-            print("False") """
