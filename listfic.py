048e23a8e8da065a66d5ad64460db655572d8134
numero6 <numero6@therover.thevillage>
2023-01-29 01:19:30 +0100

    tuning du fichier listfic.py - Utilisable

--- listfic.py
+++ listfic.py
@@ -16,28 +16,35 @@ print(list_fic)
 # print(list_fic_cover)
 fic_cover = sorted(glob.glob(motif_cover))
 fic_book = sorted(glob.glob(motif_book))
-# my_books = {}
-# my_ebooks = {}
-book_items = [{'libraryName': 'My Library'}]
+# book_items = [{'libraryName': 'My Library'}]
 my_ebooks = {}
 # my_ebooks["shelf"] = list()
+my_ebooks_items = []
 
-""" with open("list_fic.json", "w") as lf:
-    my_books["libraryName"] = "My Library" """
-for cover, ebook in zip(fic_cover, fic_book):
-        fic_ebook_cover = os.path.basename(cover)
-        fic_ebook = os.path.basename(ebook)
-        # my_ebooks.clear()
-        # my_ebooks["couvFile"] = os.path.basename(cover)
-        # my_ebooks["bookFile"] = os.path.basename(ebook)
-        print(fic_ebook_cover)
-        my_ebooks["couvFile"] = fic_ebook_cover
-        print(fic_ebook)
-        book_items.append(my_ebooks)
+with open("list_fic.json", "w") as lf:
         
-        print(book_items)
-        # json.dump(my_books, lf)
-        # print(my_books)
+        for cover, ebook in zip(fic_cover, fic_book):
+            fic_ebook_cover = os.path.basename(cover)
+            fic_ebook = os.path.basename(ebook)
+            fic_ebook_title = fic_ebook_cover.replace("_", " ")
+            fic_ebook_title = fic_ebook_title.replace(".jpg", "")
+            # my_ebooks.clear()
+            # my_ebooks["couvFile"] = os.path.basename(cover)
+            # my_ebooks["bookFile"] = os.path.basename(ebook)
+            print(fic_ebook_title)
+            print(fic_ebook_cover)
+            print(fic_ebook)
+            my_ebooks["ebookTitle"] = fic_ebook_title
+            my_ebooks["couvFile"] = fic_ebook_cover
+            my_ebooks["ebookFile"] = fic_ebook
+            # book_items.append(my_ebooks)
+            # print(type(my_ebooks))
+            # print(book_items)
+            # my_ebooks_items.append(my_ebooks)
+            # obj = json.dumps(my_ebooks)
+            # print(obj)
+            json.dump(my_ebooks, lf)
+                # print(my_books)
 
 """  my_books["ebookCover"] = fic_ebook_cover
     my_books["ebookFile"] = fic_ebook
