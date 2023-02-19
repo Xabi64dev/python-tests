19096047911f5d60385cd9d28e36695a4974e41c
numero6 <numero6@therover.thevillage>
2023-02-19 09:33:36 +0100

    Fonction cherche(moti)

--- data_read_cdc_mpj.py
+++ data_read_cdc_mpj.py
@@ -2,27 +2,16 @@
 # coding: utf-8
 
 import csv
-
-with open('data_cdc_mpj.csv') as fichier_csv:
-    # taille = len(fichier_csv.readlines())
-    reader = csv.DictReader(fichier_csv,delimiter=',')
-    motif = input("Search motif, ... ref or category : ")
-    
-    # taille = len(fichier_csv.readlines())
-    # print(taille)
-    for ligne in reader:
-        # print(len(ligne))
-        # print(ligne)
-        valeurs = ligne.values()
-        art = ligne.items()
-        liste_valeurs = list(valeurs)
-        """ print(valeurs)
-        print(art)
-        print(liste_valeurs) """
-        # print("-" * 31 + liste_valeurs[2])
-        for val in liste_valeurs:
-            if motif in val:
-                print("--> " + "Ref: " + ligne['ref'] + "\nCategory: " + ligne['cat'] + "\nMaterial:\n" + ligne['material'] + "\n" + "*" * 31)
-            # print(ligne["ref"] + ligne['cat'] + ligne['material'] )
-            else:
-                continue
+motif = input("Search motif, ... ref or category : ")
+def cherche(motif):
+    with open('data_cdc_mpj.csv') as fichier_csv:
+        reader = csv.DictReader(fichier_csv,delimiter=',')
+        for ligne in reader:
+            valeurs = ligne.values()
+            liste_valeurs = list(valeurs)
+            for val in liste_valeurs:
+                if motif in val:
+                    print("\n--> " + "Ref: " + ligne['ref'] + "\nCategory: " + ligne['cat'] + "\nMaterial:\n" + ligne['material'] + "\n" + "*" * 31)
+                else:
+                    continue
+cherche(motif)
